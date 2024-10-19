5a0723fa691745e8cda7d02ee5a1776d3d02fca2
Danny <51312422+dchenz@users.noreply.github.com>
2024-10-19 15:05:10 +0000

    Allow trilium password to be used to generate ETAPI tokens

--- trilium.py
+++ trilium.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from trilium_client import trilium_client
 from utils import loadRequiredEnv
@@ -6,14 +7,27 @@ from utils import loadRequiredEnv
 logger = logging.getLogger(__name__)
 
 
-client = trilium_client.DefaultApi(
-    trilium_client.ApiClient(
+def createClient() -> trilium_client.DefaultApi:
+    apiClient = trilium_client.ApiClient(
         configuration=trilium_client.Configuration(
             host=loadRequiredEnv("TRILIUM_URL"),
         ),
-        header_name="Authorization",
-        header_value=loadRequiredEnv("TRILIUM_TOKEN"),
-    ),
-)
+    )
+    client = trilium_client.DefaultApi(apiClient)
+    token = None
+
+    password = os.getenv("TRILIUM_PASSWORD")
+    if password:
+        response = client.login(trilium_client.LoginRequest(password=password))
+        token = response.auth_token
+
+    if not token:
+        token = loadRequiredEnv("TRILIUM_TOKEN")
+
+    apiClient.default_headers["Authorization"] = token
+    return client
+
+
+client = createClient()
 
 logger.info(client.get_app_info())
