aca1795d27e4b7a4fde19dacc5b04938267ebb0c
Danny <51312422+dchenz@users.noreply.github.com>
2024-10-20 07:11:05 +0000

    Delete unused code after adding pagination

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,5 @@
 import os
 import sys
-from typing import Any
-
-import tabulate
 
 
 def loadRequiredEnv(name: str) -> str:
@@ -14,22 +11,6 @@ def loadRequiredEnv(name: str) -> str:
     if not value:
         sys.exit(f"Missing {name} environment variable")
     return value
-
-
-def formatAsTable(records: list[dict[str, Any]], fieldsToPick: list[str]) -> str:
-    """
-    Formats a list of dictionaries into a table, assuming all dictionaries have the same keys.
-    """
-    headers = {field: field for field in fieldsToPick}
-    formatted = tabulate.tabulate(records, headers=headers)
-    return f"```\n{formatted}\n```"
-
-
-def pickDictKeys(record: dict[str, Any], fields: list[str]) -> dict[str, Any]:
-    """
-    Returns a new dictionary containing only a selected set of keys.
-    """
-    return {k: record[k] for k in record if k in fields}
 
 
 def partitionList(xs: list, n: int) -> list:
